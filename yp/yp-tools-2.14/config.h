6d83def59e65d031ba5f632a6c77163b695c4ab7
Nicholas Christopoulos <nereus@freemail.gr>
2020-08-01 06:42:54 +0300

    quick and dirty fix

---
+++ yp/yp-tools-2.14/config.h
@@ -0,0 +1,117 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Define to 1 if translation of program messages to the user's native
+   language is requested. */
+#define ENABLE_NLS 1
+
+/* Define to 1 if you have the MacOS X function CFLocaleCopyCurrent in the
+   CoreFoundation framework. */
+/* #undef HAVE_CFLOCALECOPYCURRENT */
+
+/* Define to 1 if you have the MacOS X function CFPreferencesCopyAppValue in
+   the CoreFoundation framework. */
+/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+
+/* Define to 1 if you have the <crypt.h> header file. */
+#define HAVE_CRYPT_H 1
+
+/* Define if the GNU dcgettext() function is already present or preinstalled.
+   */
+#define HAVE_DCGETTEXT 1
+
+/* Define to 1 if you have the `getdelim' function. */
+/* #undef HAVE_GETDELIM */
+
+/* Define to 1 if you have the `getline' function. */
+#define HAVE_GETLINE 1
+
+/* Define to 1 if you have the <getopt.h> header file. */
+#define HAVE_GETOPT_H 1
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
+/* Define if you have the iconv() function and it works. */
+/* #undef HAVE_ICONV */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `nsl' library (-lnsl). */
+#define HAVE_LIBNSL 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <rpc/clnt_soc.h> header file. */
+/* #undef HAVE_RPC_CLNT_SOC_H */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <xcrypt.h> header file. */
+/* #undef HAVE_XCRYPT_H */
+
+/* Define to 1 if you have the `ypbinderr_string' function. */
+#define HAVE_YPBINDERR_STRING 1
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "yp-tools"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "kukuk@linux-nis.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "yp-tools"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "yp-tools 2.14"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "yp-tools"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.14"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define if you want to use libcrack to check the new passwords */
+/* #undef USE_CRACKLIB */
+
+/* Define if you want to use some extra checks besides the cracklib */
+/* #undef USE_CRACKLIB_STRICT */
+
+/* Version number of package */
+#define VERSION "2.14"
+
+/* Define if yppasswd is deprecated and passwd should be used instead */
+/* #undef YPPASSWD_IS_DEPRECATED */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
